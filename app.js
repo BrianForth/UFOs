cc320d77eeb6248de7ce6d7b53c46778af5b992a
Brian Forth <brianwforth@gmail.com>
2021-03-07 17:01:53 -0600

    11.5.4 - app.py ready.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function buildTable(data) {
     data.forEach((dataRow) => {
         // Append row to table
         let row = tbody.append("tr");
-        
+
         // Loop through fields in dataRow and add each value as table cell (td)
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
@@ -21,3 +21,23 @@ function buildTable(data) {
         });
     });
 }
+
+// Function to filter data when a click is detected
+function handleClick(){
+    // Grab datetime value from filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // If a date was entered, filter the data using that date
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    //Rebuild table with filtered data
+    buildTable(filteredData);
+}
+
+// Listen for a click, and call handleClick when it happens
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Populate table with original data on page load
+buildTable(tableData);
